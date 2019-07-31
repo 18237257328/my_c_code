dae16f9d8e626262ce6773311e7e5e100635cbfe
18237257328 <2972866128@qq.com>
2019-07-31 09:38:30 +0800

    第二次提交

---
+++ 未命名2.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+	int a[10];
+    for (int i=0;i<4;i++) {
+    	scanf ("%d",&a[i]);
+	}
+	for (i=0;i<4;i++) printf ("%d ",a[i]);
+	return 0;
+}
