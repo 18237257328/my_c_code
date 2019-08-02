d3755f87927f8d1bda842a3cc38d2164af86f49c
18237257328 <2972866128@qq.com>
2019-08-02 22:34:34 +0800

    十进制数转八进制数

---
+++ 14.cpp
@@ -0,0 +1,22 @@
+/*
+蓝桥杯题目 算法训练 十进制数转八进制数  
+时间限制：1.0s   内存限制：512.0MB
+    
+　　编写函数把一个十进制数输出其对应的八进制数。
+样例输入
+9274
+样例输出
+22072 
+*/ 
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(void) {
+     int n;
+     scanf ("%d",&n);
+     printf ("%o",n);
+    
+    return 0;
+}
+
